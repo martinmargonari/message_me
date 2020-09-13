6e79397aacf0c16ac5a1521ace1bfeb36a71e717
Martin Margonari <martinmargonari@gmail.com>
2020-09-13 17:00:44 -0300

    Configured action cable for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.web_socket_server_url = "wss://message-me-mmargonari.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = ['https://message-me-mmargonari.herokuapp.com', 'http://message-me-mmargonari.herokuapp.com']
+
 end
